8c72a7aec62021bdc0a164d6d860d80ba35ec1cc
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-07-11 22:30:24 +0100

    Implement dynamic rendering of To Do List and styling with Webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 import './style.css';
 
-console.log('hello!!');
+const toDoTasks = [
+  {
+    description: 'Task 1',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Task 2',
+    completed: true,
+    index: 2,
+  },
+  {
+    description: 'Task 3',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'Task 4',
+    completed: false,
+    index: 4,
+  },
+];
+
+function renderTasks() {
+  const list = document.querySelector('.list');
+
+  list.innerHTML = '';
+
+  toDoTasks.forEach((task) => {
+    const listItem = `<li class="item">
+                        <input
+                        type="checkbox"
+                        value=${task.completed}
+                        />
+                        <span>
+                        ${task.description}
+                        </span>
+                        <button class="btn">❌</button>
+                     </li>`;
+
+    list.insertAdjacentHTML('beforeend', listItem);
+  });
+}
+
+renderTasks();
