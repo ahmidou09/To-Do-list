5cb6cdcbba2af185cdb0acc202af62e8bcca023f
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-07-12 19:04:25 +0100

    Implement new functionality for the To Do List

--- src/index.js
+++ src/index.js
@@ -1,47 +1,6 @@
 import './style.css';
+import TodoList from './todoList.js';
 
-const toDoTasks = [
-  {
-    description: 'Task 1',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'Task 2',
-    completed: true,
-    index: 2,
-  },
-  {
-    description: 'Task 3',
-    completed: false,
-    index: 3,
-  },
-  {
-    description: 'Task 4',
-    completed: false,
-    index: 4,
-  },
-];
+const task = new TodoList();
 
-function renderTasks() {
-  const list = document.querySelector('.list');
-
-  list.innerHTML = '';
-
-  toDoTasks.forEach((task) => {
-    const listItem = `<li class="item">
-                        <input
-                        type="checkbox"
-                        value=${task.completed}
-                        />
-                        <span>
-                        ${task.description}
-                        </span>
-                        <button class="btn">❌</button>
-                     </li>`;
-
-    list.insertAdjacentHTML('beforeend', listItem);
-  });
-}
-
-renderTasks();
+task.renderTasks();
